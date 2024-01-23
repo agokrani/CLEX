faf5f940f3bb95a10a983585758292fb2f950943
Aman Gokrani <amaan.gokrani@gmail.com>
2024-01-23 12:03:10 +0000

    phi2-exp train_lm change

--- scripts/train_lm.sh
+++ scripts/train_lm.sh
@@ -1,26 +1,26 @@
-torchrun --nproc_per_node=8 \
+torchrun --nproc_per_node=1 \
 --master_port=2349 \
 train/train_lm.py \
-    --model_name_or_path /path/to/basemodel  \
-    --data_path /path/to/lm_data \
+    --model_name_or_path "DAMO-NLP-SG/CLEX-Phi-2-32K"  \
+    --data_path  "/workspace/git/LongQLoRA/data/pretrain/iCliniq_1k.jsonl" \
     --bf16 True \
-    --output_dir /path/to/save_dir \
+    --output_dir "/workspace/models/phi2-32k-pretrain-HC" \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 200 \
-    --save_total_limit 10 \
-    --max_steps xxx \
+    --save_total_limit 2 \
+    --max_steps -1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 4096 \
+    --model_max_length 8192 \
     --gradient_checkpointing True \
     --ddp_find_unused_parameters True \
     --do_train True \
